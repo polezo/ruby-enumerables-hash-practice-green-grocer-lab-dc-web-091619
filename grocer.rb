a5194fa495ceae5133e147d15633ec787d7742ee
Adam Finch <atyreefinch@gmail.com>
2019-08-12 00:50:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -38,12 +38,16 @@ end
 
 def apply_clearance(cart)
   cart.keys.each do |memo|
-    if cart[memo][:clearance]
-      cart[memo][:price] *= 0.80
-      cart[memo][:price] = cart[memo][:price].round(2)
+   if cart[item][:clearance]
+      cart[item][:price] = (cart[item][:price]*0.80).round(2)
     end
   end
 end
+
+  cart.keys.each do |item|
+    
+  end
+  cart
 
 def checkout(cart, coupons)
   coupons_cart = consolidate_cart(cart)
