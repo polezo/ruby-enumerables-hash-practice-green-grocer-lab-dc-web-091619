fe4312266a8dfa7a9112b383c52c81adfbc1450b
Adam Finch <atyreefinch@gmail.com>
2019-08-12 00:30:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,48 @@
 def consolidate_cart(cart)
-  # code here
+  consolidated_cart = {}
+  cart.each do |memo|
+    k,v = memo.first
+    if consolidated_cart[k]
+        consolidated_cart[k][:count] += 1
+    else
+      consolidated_cart[k] = {
+        :price => v[:price],
+        :clearance => v[:clearance],
+        :count => 1
+      }
+    end
+  end
+  consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |memo| 
+   if cart.keys.any? memo[:item]
+     if cart[memo[:item]][:count] >= memo[:num]
+       new_item = "#{memo[:item]} W/COUPON"
+       if cart[new_item]
+          cart[new_item][:count] += memo[:num]
+        else
+            cart[new_item] = {
+            count: memo[:num],
+            price: memo[:cost]/memo[:num],
+            clearance: cart[memo[:item]][:clearance]
+       }
+        end
+        cart[memo[:item]][:count]-=memo[:num]
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.keys.each do |memo|
+    if cart[memo][:clearance]
+      cart[memo][:price] = 0.8*cart[memo][:price]
+      cart[memo][:price].round(2)
+    end
+  end
 end
 
 def checkout(cart, coupons)
